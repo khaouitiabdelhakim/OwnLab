29a56f1ccdde39efdc4a996806fb8194a9cb156b
abdelhakimkhaouiti <abdelhakimkhaouiti62@gmail.com>
2023-10-21 21:57:55 +0100

    Initial commit

---
+++ OwnLab/src/main/java/com/abdelhakim/ownlab/Labs.java
@@ -0,0 +1,16 @@
+package com.abdelhakim.ownlab;
+
+public class Labs {
+
+    private String key = "ala34dgr23";
+    public Labs() {
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+}
